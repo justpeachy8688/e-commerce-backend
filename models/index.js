89f408875f0ee3c438a94df34fad0ef0368a9452
Taylor Sanders <tsanders3515@gmail.com>
2021-03-21 21:09:40 -0800

    set up Associations + foreignKeys

--- models/index.js
+++ models/index.js
@@ -3,11 +3,28 @@ const Product = require('./Product');
 const ProductTag = require('./ProductTag');
 const Tag = require('./Tag');
 
-// Product.belongsToMany(Tag, {
-//     through: {
-//         model: ProductTag,
-//         unique: false
-//     },
-// })
+Product.belongsTo(Category, {
+    foreignKey: 'category_id',
+});
+
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+});
+
+Product.hasOne(Category)
+
+Product.belongsToMany(Tag, {
+    through: {
+        model: ProductTag,
+        foreignKey: 'product_id'
+    }
+})
+
+Tag.belongsToMany(Product, {
+    through: {
+        model: ProductTag,
+        foreignKey: 'tag_id'
+    }
+})
 
 module.exports = { Category, Product, ProductTag, Tag };
